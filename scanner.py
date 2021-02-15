3511c0fdeedc6f1137536142455dbd7e2e9d4d4f
jacklmbrt07 <jacklmbrt07@gmail.com>
2021-02-15 09:12:57 -0800

    first commit

---
+++ scanner.py
@@ -0,0 +1,15 @@
+import nmap
+
+nmap_path = r""
+scanner = nmap.PortScanner()
+
+print("Welcome, this is a simple nmap automation tool")
+print("<------------------------------------------------------>")
+
+ip_addr = input("Please enter the IP address you want to scan: ")
+print("The IP you entered is: ", ip_addr)
+type(ip_addr)
+
+resp = input("\nPlease enter the type of scan you want to run\n1) SYN ACK Scan\n2) UDP Scan\n3) Comprehensive scan")
+print("You hace selected option: ", resp)
+
