a2f56803c5bc6da5d1a374b7df5148f0e2d6f35d
jacklmbrt07 <jacklmbrt07@gmail.com>
2021-02-15 10:26:07 -0800

    solved error by brew install nmap

--- scanner.py
+++ scanner.py
@@ -1,6 +1,6 @@
 import nmap
 
-nmap_path = r""
+# nmap_path = r""
 scanner = nmap.PortScanner()
 
 print("Welcome, this is a simple nmap automation tool")
@@ -13,3 +13,6 @@ type(ip_addr)
 resp = input("\nPlease enter the type of scan you want to run\n1) SYN ACK Scan\n2) UDP Scan\n3) Comprehensive scan")
 print("You hace selected option: ", resp)
 
+if resp == '1':
+    print("Nmap Version: ", scanner.nmap_version())
+    scanner.scan()
